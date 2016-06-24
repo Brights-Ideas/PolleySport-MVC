b0a658413320114c88354b770495e47bdef4e9f0
Brighty28 <abright2012@gmail.com>
2016-06-24 17:11:07 +0100

    Check out process
    Updated checkout page styling

--- PolleySport/Controllers/CheckoutController.cs
+++ PolleySport/Controllers/CheckoutController.cs
@@ -6,6 +6,7 @@ using PolleySport.Models;
 namespace PolleySport.Controllers
 {
     //[Authorize]
+    [AllowAnonymous]
     public class CheckoutController : Controller
     {
         PolleySportEntities storeDB = new PolleySportEntities();
